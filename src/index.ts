67e5bf28b241aeaccc42cc20d7cab0096aaf3d46
BrunoTrindade40 <brunotrindade_40@hotmail.com>
2025-05-14 10:38:03 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log(`Hello World`);
+import { config } from 'dotenv';
+import express from "express";
+
+config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.get("/api/hello/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor funcionando na porta ${port}`);
+});
